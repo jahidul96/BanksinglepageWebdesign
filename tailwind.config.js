6e71c57bb49119e575a6b6b4fa3bb134215fdb53
jahidul islam <Jahidul96@yahoo.com>
2022-05-12 20:41:37 +0600

    added files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        tomato: 'tomato',
+        redTomato: '#FF4949',
+        light: '#E8F9FD',
+      }
+    },
+  },
+  plugins: [],
+}
+
